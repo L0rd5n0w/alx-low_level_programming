cdafca62e2f0e031f0617bdbb84038d4d78bc456
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-10 20:01:02 +0100

    argh

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - display output using printf
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return(0);
+}
