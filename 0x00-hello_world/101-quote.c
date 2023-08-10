30fae6e52405701972c523f156d853f038328a5b
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-10 20:59:05 +0100

    HELPFUL FRIEND

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
-
-int main() {
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, sizeof(message) - 1);
-    return 1;
+/**
+* main - A C programme that prints a line to standard
+*
+* Return: Always 1 (Success)
+*/
+int main(void)
+{
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, sizeof(message) - 1);
+	return (1);
 }
