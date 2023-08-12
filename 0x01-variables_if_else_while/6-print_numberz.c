f799503d32b7c0c9df8c06786b77300f5dfdcf3d
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-12 12:57:48 +0100

    ....

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main -code to display single digits in base10
+ *
+ * Return:(0) Always
+ */
+int main(void)
+{
+	int q;
+
+	for (q = 48; q < 58; q++)
+	{
+		putchar(q);
+	}
+	putchar('\n');
+
+	return (0);
+}
