00d16ed094504bab4a94e654df734e4d275894e4
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-12 12:49:48 +0100

    loophole

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main -code to display single digits in base10
+ *
+ * Return:(0) Always
+ */
+int main(void)
+{
+	int q;
+
+	for (q = 0; q <= 9; q++)
+	{
+		printf("%d", q);
+	}
+	printf("\n");
+
+	return (0);
+}
