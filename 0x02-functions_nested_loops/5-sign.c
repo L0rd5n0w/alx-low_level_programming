988d980f4321a8a104e75550ca7a998acc1a5999
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-16 11:10:25 +0100

    .

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_sign -Code to check
+ *@n: parameter to check
+ *Return: (0) Always
+ */
+int print_sign(int n)
+{
+	if (n > '0')
+		_putchar('+');
+		return (1);
+	else if (n < '0')
+		_putchar('-');
+		return (-1);
+	else if (n == '0')
+		_putchar('0');
+		return (0);
+}
