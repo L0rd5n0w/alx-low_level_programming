da2470a88584f3c2d4d1520521e2b820a2d91630
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-16 09:48:00 +0100

    .

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ *print_alphabet -code to print alphabet
+ *Return: (0) Always
+ */
+void print_alphabet(void);
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
