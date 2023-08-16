5ccacc6b6a9c4c04c0279f27bd7fbb18bdd5f15c
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-16 10:22:38 +0100

    .

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include "main.h"
 /**
- *print_alphabet -code to print alphabet
+ *print_alphabet_x10 -code to print alphabet multiple times
  *Return: (0) Always
  */
 void print_alphabet_x10(void)
@@ -12,5 +12,5 @@ void print_alphabet_x10(void)
 	for (z = 0; z <= 9; z++)
 		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
-	_putchar('\n');
+		_putchar('\n');
 }
