a22637de5746402e62cb88a66054a44149013b62
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-16 11:53:17 +0100

    .

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add -code to check
+ *Return:(0) Always
+ *@a: first parameter
+ *@b: second parameter
+ */
+int add(int a, int b)
+{
+	int kol;
+
+	kol = a + b;
+	_putchar(kol);
+}
