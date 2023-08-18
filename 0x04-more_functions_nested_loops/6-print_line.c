1a39d544ab6828b8286c5c3c0e85866c860aa02b
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-18 09:59:12 +0100

    .

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *print_line -code to print
- *Return:(0)Always
+ *Return:void
  *@n: parameter
  */
 void print_line(int n)
@@ -9,15 +9,11 @@ void print_line(int n)
 	int c;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
-		for (c = 0; c <= n; c++)
-		{
+		for (c = 1; c <= n; c++)
 			_putchar('_');
-		}
 		_putchar('\n');
 	}
 }
