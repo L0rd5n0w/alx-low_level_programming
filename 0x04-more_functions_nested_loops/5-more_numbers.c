e6e3683579b1d576c1f7e21b0b2b4e74b12033d2
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-18 04:45:24 +0100

    .

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *more_numbers -code to check
+ *Return:(0)Always
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int c = 0;
+
+	while (c <= 9)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i)
+		}
+		c++
+	}
+	_putchar('\n')
+}
