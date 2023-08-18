804a6c124ff9564a3f851a3f75dbe6aba776b262
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-18 04:29:48 +0100

    .

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#inlclude "main.h"
+/**
+ *print_numbers -code to check
+ *Return:(0) Always
+ *
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
