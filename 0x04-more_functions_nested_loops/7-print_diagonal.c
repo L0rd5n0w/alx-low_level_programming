f088bb2d1fc44cd355651b68caa7624c81f7344f
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-18 10:23:54 +0100

    .

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *print_diagonal -code to check
+ *Return void
+ *@n: parameter
+ */
+void print_diagonal(int n)
+{
+	int c;
+	int t;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (c = 0; c <= n; c++)
+		{
+			_putchar('\\');
+			_putchar('\n');
+			for (t = 1; t <= c; t++)
+			{
+				_putchar(' ');
+			};
+		}
+	}
+}
