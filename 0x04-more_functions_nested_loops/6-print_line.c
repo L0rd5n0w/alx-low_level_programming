3fbf695621c3f8bd36bc595ed18ca82c16ea953d
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-18 09:56:58 +0100

    .

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,13 +8,16 @@ void print_line(int n)
 {
 	int c;
 
-	for (c = 0; c <= n; c++)
+	if (n <= 0)
 	{
-		if (n <= 0)
-		{
-			break;
-		}
-		_putchar('_');
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (c = 0; c <= n; c++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
