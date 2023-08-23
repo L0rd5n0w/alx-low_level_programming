c49281950e6f39afb465ece885a2f21e5529e4d3
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-23 12:37:54 +0100

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *reverse_array - function to reverse an array
+ *Return:(0)Always
+ *@a: parameter
+ *@n: parameter
+ */
+void reverse_array(int *a, int n)
+{
+	int e = 0;
+	int q;
+
+	for (; e <= n-- ; e++)
+	{
+		q = a[e];
+		a[e] = a[n];
+		a[n] = q;
+	}
+}
