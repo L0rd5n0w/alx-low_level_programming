1a0a0363bb936a5f27612d50dcf4a4f1beab7c50
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-23 13:43:32 +0100

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *cap_string - Capitalizes all words of a string
+ *@str: char param
+ *Return: 0
+ */
+
+char *cap_string(char *ret)
+{
+	int i;
+	int ploy = 1;
+
+	for (i = 0; ret[i] != '\0'; i++)
+	{
+	if (ploy && isalpha(ret[i]))
+	{
+	ret[i] = toupper(ret[i]);
+	}
+
+	ploy = 0;
+	switch (ret[i])
+	{
+	case ' ':
+	case '\t':
+	case '\n':
+	case ',':
+	case ';':
+	case '.':
+	case '!':
+	case '?':
+	case '"':
+	case '(':
+	case ')':
+	case '{':
+	case '}':
+	ploy = 1;
+	break;
+	default:
+	break;
+	}
+	}
+
+	return (ret);
+}
