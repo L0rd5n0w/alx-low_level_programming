8a05e96a309a78141e2c79b9bdb774b798755fbb
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-23 13:13:35 +0100

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ **string_toupper -function to change string to upper
+ *Return:(0)Always
+ *
+ */
+char *string_toupper(char *s)
+{
+	char *rt = s;
+
+	while (*rt != '\0')
+	{
+		if (*rt >= 'a' && *rt <= 'z')
+		{
+			*rt = *rt - ('a' - 'A');
+		}
+		rt++;
+	}
+	return (s);
+}
