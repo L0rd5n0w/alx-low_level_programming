19855069d07d653726dc0fee208e1fff6f3b2d52
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2023-08-24 09:54:02 +0100

    error handling

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,39 +2,31 @@
 /**
  *leet  - conversion to 7331
  *Return:(0)Always
- *
+ *@str: parameter
  */
-
-char *leet(char *str) 
+char *leet(char *str)
 {
-	char *ptr = str;
+	int i;
+	char leet_[256] = {0};
 
+	leet_['a'] = '4';
+	leet_['A'] = '4';
+	leet_['e'] = '3';
+	leet_['E'] = '3';
+	leet_['o'] = '0';
+	leet_['O'] = '0';
+	leet_['t'] = '7';
+	leet_['T'] = '7';
+	leet_['l'] = '1';
+	leet_['L'] = '1';
 
-	while (*ptr != '\0') 
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		char c = *ptr;
-
-		if (c == 'a' || c == 'A')
+		if (leet_[(unsigned char) str[i]])
 		{
-			*ptr = '4';
+			str[i] = leet_[(unsigned char) str[i]];
 		}
-		else if (c == 'e' || c == 'E')
-		{
-			*ptr = '3';
-		}
-		else if (c == 'o' || c == 'O')
-		{
-			*ptr = '0';
-		}
-		else if (c == 't' || c == 'T')
-		{
-			*ptr = '7';
-		}
-		else if (c == 'l' || c == 'L')
-		{
-			*ptr = '1';
-		}
-		ptr++;
 	}
+
 	return (str);
 }
